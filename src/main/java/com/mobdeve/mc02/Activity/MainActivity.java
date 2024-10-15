2f13cabb9c711760f01055dde36e505fe36eed15
Polo-RalphLauren <165652097+Polo-RalphLauren@users.noreply.github.com>
2024-10-16 00:12:51 +0800

    Update MainActivity.java

--- src/main/java/com/mobdeve/mc02/Activity/MainActivity.java
+++ src/main/java/com/mobdeve/mc02/Activity/MainActivity.java
@@ -15,8 +15,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        
         ConstraintLayout btn1 = findViewById(R.id.btn1);
         btn1.setOnClickListener(v -> startActivity(new Intent(MainActivity.this, TaskListActivity.class)));
+        
+        ConstraintLayout plannerButton = findViewById(R.id.btn2);  // Assuming 'btn2' is the Planner button's ID
+        plannerButton.setOnClickListener(v -> startActivity(new Intent(MainActivity.this, PlannerActivity.class)));
     }
 }
