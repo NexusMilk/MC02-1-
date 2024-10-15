d1b2dcd1117b2c1666a156db7dd2d421fa377c2f
Polo-RalphLauren <165652097+Polo-RalphLauren@users.noreply.github.com>
2024-10-16 00:58:33 +0800

    Update PlannerActivity.java

--- src/main/java/com/mobdeve/mc02/Activity/PlannerActivity.java
+++ src/main/java/com/mobdeve/mc02/Activity/PlannerActivity.java
@@ -1,6 +1,9 @@
 package com.mobdeve.mc02.Activity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import androidx.appcompat.app.AppCompatActivity;
 import com.mobdeve.mc02.R;
 
@@ -10,6 +13,16 @@ public class PlannerActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_planner);
-        
+
+        // Set up floating action button click listener
+        FloatingActionButton fabAddTask = findViewById(R.id.fab_add_task);
+        fabAddTask.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Navigate to AddTaskActivity or show task creation dialog
+                Intent intent = new Intent(PlannerActivity.this, AddTaskActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
