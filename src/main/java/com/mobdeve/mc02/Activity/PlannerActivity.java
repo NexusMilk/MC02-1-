4c22f97a9eaacdf48917bef2bb6c97ac385c1a60
Polo-RalphLauren <165652097+Polo-RalphLauren@users.noreply.github.com>
2024-10-16 00:12:15 +0800

    Create PlannerActivity.java

---
+++ src/main/java/com/mobdeve/mc02/Activity/PlannerActivity.java
@@ -0,0 +1,15 @@
+package com.mobdeve.mc02.Activity;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import com.mobdeve.mc02.R;
+
+public class PlannerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_planner);
+        
+    }
+}
